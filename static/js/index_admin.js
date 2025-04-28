818b6cf5c0152ffa36b134fe83c5baee0860acd8
Chasan123 <josecsusat@gmail.com>
2025-04-28 11:50:30 -0500

    Update index_admin.js

--- static/js/index_admin.js
+++ static/js/index_admin.js
@@ -9,3 +9,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btnHamburguesa = document.getElementById('btnHamburguesa');
+    const nav = document.getElementById('adminNav');
+    const btnUserMenu = document.getElementById('btnUserMenu');
+    const userMenu = document.getElementById('userMenu');
+
+    if (btnHamburguesa) {
+        btnHamburguesa.addEventListener('click', function () {
+            nav.classList.toggle('admin-nav--active');
+        });
+    }
+
+    btnUserMenu.addEventListener('click', function () {
+        userMenu.classList.toggle('admin-header__user-menu--active');
+    });
+
+    document.addEventListener('click', function(e) {
+        if (!btnUserMenu.contains(e.target) && !userMenu.contains(e.target)) {
+            userMenu.classList.remove('admin-header__user-menu--active');
+        }
+    });
+});
