1fecff24bcfbcd0e1557cadf69f792fc37dafc72
DavDCV <caballerovasquezdavidantonio@gmail.com>
2025-06-08 06:18:34 -0500

    Cambio de diseño en el catálogo y cambio de colores de la páginas de cliente

--- static/js/catalogo.js
+++ static/js/catalogo.js
@@ -1,31 +1,44 @@
-const stateCatalogCategory = "catalog__category--active";
-const stateProductInvisible = "catalog_product--invisible";
+const stateProductInvisible = "ctg__product--invisible";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const listCategorias = document.querySelectorAll('.catalog__category');
-    const productos = document.querySelectorAll('.catalog__product');
+    const listCategorias = document.querySelectorAll('.ctg__filters-value input[type="checkbox"]');
+    const productos = document.querySelectorAll('.ctg__product');
+    var search_query = document.querySelector('#search_query');
 
-    listCategorias.forEach((categoria, index) => {
-        categoria.addEventListener('click', () => {
-            const nombre_categoria = categoria.textContent.trim();
-            
-            listCategorias.forEach(c => c.classList.toggle(stateCatalogCategory, c === categoria));
-            
-            productos.forEach(producto => {
-                const categoria_producto = producto.dataset.categoria.trim();
-                const isMatchingCategory = nombre_categoria === categoria_producto || index === 0;
-                producto.classList.toggle(stateProductInvisible, !isMatchingCategory);
+    function obtenerCategoriasSeleccionadas() {
+        return Array.from(listCategorias).filter(categoria => categoria.checked).map(categoria => categoria.value);
+    }
+
+    function switchVisibilidadProductos() {
+        listCategorias.forEach((categoria, index) => {
+            categoria.addEventListener('change', () => {
+                if (index === 0) {
+                    Array.from(listCategorias).map(categoria => categoria.checked = false);
+                    Array.from(productos).map(producto => producto.classList.remove(stateProductInvisible));
+                    return categoria.checked = true;
+                }
+
+                let categoriasSeleccionadas = obtenerCategoriasSeleccionadas();
+
+                productos.forEach(producto => {
+                    let categoriaProducto = producto.dataset.categoria.trim();
+                    let estaSeleccionada = categoriasSeleccionadas.includes(categoriaProducto);
+                    producto.classList.toggle(stateProductInvisible, !estaSeleccionada);
+                });
             });
         });
-    });
+    }
+
+    if (search_query) filtrarProductoPorNombre(search_query.textContent);
+    switchVisibilidadProductos();
 });
 
 export function filtrarProductoPorNombre(nombreProducto = '') {
-    const productos = document.querySelectorAll('.catalog__product');
+    const productos = document.querySelectorAll('.ctg__product');
     const query = nombreProducto.toLowerCase();
 
     productos.forEach(producto => {
-        const productName = producto.querySelector('.catalog__product-name').textContent.toLowerCase();
+        const productName = producto.querySelector('.ctg__product-name').textContent.toLowerCase();
         const isMatch = productName.includes(query);
         producto.classList.toggle(stateProductInvisible, !isMatch);
     });
