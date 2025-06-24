7897f7fe415e218e19254fe2819c5806e18da930
SaucedoRuiz <131692149+SaucedoRuiz@users.noreply.github.com>
2025-06-24 14:25:08 -0500

    admin4

--- controladores/controlador_clientes_pagos_pendientes.py
+++ controladores/controlador_clientes_pagos_pendientes.py
@@ -5,18 +5,22 @@ def obtener_clientes_con_pagos_pendientes():
     try:
         with conexion.cursor() as cursor:
             query = """
-                SELECT 
+               SELECT 
                     p.id_pedido,
                     p.fecha_registro,
                     CONCAT(per.nombre, ' ', per.ape_paterno, ' ', per.ape_materno) AS nombre_completo,
                     per.numero_telefono AS telefono,
-                    COALESCE(SUM(pg.monto), 0) AS monto_pagado
+                    CASE 
+                        WHEN COALESCE(SUM(pg.monto), 0) >= COALESCE(SUM(dp.precio), 0) THEN 'Pagado'
+                        ELSE CONCAT('S/ ', COALESCE(SUM(dp.precio), 0) - COALESCE(SUM(pg.monto), 0))
+                    END AS monto_pagado
                 FROM PEDIDO p
                 LEFT JOIN PAGO pg ON p.id_pedido = pg.id_pedido
                 INNER JOIN PERSONA per ON p.id_persona = per.id_persona
+                LEFT JOIN PRODUCTO dp ON p.id_pedido = dp.id_producto
                 GROUP BY p.id_pedido
-                HAVING monto_pagado = 0
-                ORDER BY p.fecha_registro DESC
+                ORDER BY p.fecha_registro DESC;
+
             """
             cursor.execute(query)
             columnas = [col[0] for col in cursor.description]
