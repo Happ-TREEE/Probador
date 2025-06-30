2a84fc35a29e4bc0d73e91fd20f0d4a50c9995ac
DavDCV <caballerovasquezdavidantonio@gmail.com>
2025-06-30 10:56:28 -0500

    Arreglando el pago

--- routers/router_pago.py
+++ routers/router_pago.py
@@ -4,16 +4,22 @@ import controladores.controlador_pago as controlador_pago
 
 router_pago = Blueprint("router_pago", __name__)
 
+
 @router_pago.route("/pago/registrar", methods=["POST"])
-@autenticacion_requerida(tipo_usuario=0)  # 0 = cualquier usuario logueado
 def registrar_pago():
     data = request.get_json() or {}
+
     success = controlador_pago.registrar_pedido_con_pago(
-        id_persona=data.get("id_persona"),
+        numero_documento=data.get("numero_documento"),
+        tipo_documento=data.get("tipo_documento"),
+        tipo_persona=data.get("tipo_persona"),
+        nombre_cliente=data.get("nombre_cliente"),
+        email=data.get("email"),
         monto=data.get("monto"),
         id_tipo_pago=data.get("id_tipo_pago"),
         medio=data.get("medio"),
         productos=data.get("productos", []),
         codigo_verificacion=data.get("codigo_verificacion"),
     )
+
     return jsonify({"success": success})
